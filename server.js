0285131440fd59c53467d39b4554a1c58c3e7a6d
daxkongjh <dax.k@live.com>
2022-06-13 15:55:41 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,26 @@
+//DEPENDENCIES
+require("dotenv").config()
+const express = require("express")
+const app = express()
+const mongooose = require("mongoose")
+mongoURI= process.env.MONGO_URI
+
+
+//CONFIG
+port = process.env.PORT || 3000
+
+
+
+//LISTENER
+app.listen(port, ()=>{
+    console.log("Listening to port", port)
+})
+
+//MIDDLEWARE
+app.use(express.urlencoded({extended:true}))
+
+
+// TEST ROUTE
+app.get("/", (req, res)=>{
+    res.send("HELLO WORLD")
+})
